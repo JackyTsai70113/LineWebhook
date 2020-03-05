bbe939fbcdc48351203f90c1a27d812a1c81c742
Jacky_Home <j70113jacky@yahoo.com.tw>
2020-03-05 19:14:55 +0800

    修正註解

--- 1.PresentationLayer/UI/Website/Models/LineApiSource.cs
+++ 1.PresentationLayer/UI/Website/Models/LineApiSource.cs
@@ -6,6 +6,6 @@ namespace Website.Models
     public class ReplyMessages
     {
         public string replyToken { get; set; }
-        public List<LocationMessage> messages { get; set; }
+        public List<Message> messages { get; set; }
     }
 }
