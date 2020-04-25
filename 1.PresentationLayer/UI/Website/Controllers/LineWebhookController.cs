9a43b3a04fd902f3b84874d20ed000aed8888e18
Jacky_Home <j70113jacky@yahoo.com.tw>
2020-04-25 17:41:07 +0800

    no message

--- 1.PresentationLayer/UI/Website/Controllers/LineWebhookController.cs
+++ 1.PresentationLayer/UI/Website/Controllers/LineWebhookController.cs
@@ -21,33 +21,40 @@ using Models.Line.API;
 using Utility.Google.MapAPIs;
 using Models.Google.API;
 
-namespace Website.Controllers
-{
-    public class LineWebhookController : Controller
-    {
-        private static string channelAccessToken = 
+namespace Website.Controllers {
+
+    /// <summary>
+    /// LineWebhook控制器，Line Server 的 I/O
+    /// </summary>
+    public class LineWebhookController : Controller {
+
+        private static string channelAccessToken =
             @"tkOO80fthaESrdEWkHn5+gsypQLHd1N3DZcNsWaJku3GeO/
             HsFMyCSyU95KnA6p2bTLPFJS0y4joCknQyppqlwaDK34rrQgS
             W39EcS0j5WNEZGIlkup0nJ+xlBf+mcw89H1xKAc5Ubd0xA9/Z
             9RSIwdB04t89/1O/w1cDnyilFU=";
-        
+
+        /// <summary>
+        /// LINE Webhook的入口，負責解讀line的訊息。
+        /// </summary>
+        /// <param name="requestBody">line</param>
+        /// <returns></returns>
         [HttpPost]
-        public IActionResult Index([FromBody] dynamic requestBody)
-        {
+        public IActionResult Index([FromBody] dynamic requestBody) {
             Console.WriteLine($"==========[LineWebhook/Index]==========");
             Console.WriteLine($"From LINE SERVER");
             Console.WriteLine($"requestBody:");
             Console.WriteLine($"{requestBody}");
             Console.WriteLine($"====================");
-            
+
             RequestHandler handler = new RequestHandler(requestBody);
 
             LineRequestBody body = handler.requestBody;
             string replyToken = body.events[0].replyToken;
-            switch(handler.messageType)
-            {
-                case "text":
-                    return Content("");
+            switch (handler.messageType) {
+                //case "text":
+                //    return Content("");
+
                 case "location":
                     //Models.Line.API.LocationMessage locationMsg = (LocationMessage)(body.events[0].message);
                     string result1 = ReplyLocationMessages(replyToken, body.events[0].message.address);
@@ -61,11 +68,9 @@ namespace Website.Controllers
             return Content(requestBody.ToString() + "\n" + result);
         }
 
-        private string ReplyMessages(string replyToken, List<string> messageTexts)
-        {
+        private string ReplyMessages(string replyToken, List<string> messageTexts) {
             string result = "";
-            try
-            {
+            try {
                 string url = "https://api.line.me/v2/bot/message/reply";
                 var request = (HttpWebRequest)WebRequest.Create(url);
                 request.Method = "POST";
@@ -89,21 +94,18 @@ namespace Website.Controllers
                 //     packageId = "1",
                 //     stickerId = "1"
                 // });
-                messages.Add(new TextMessage
-                {
+                messages.Add(new TextMessage {
                     type = "text",
                     text = "test以下是離你最近的藥局"
                 });
-                messages.Add(new LocationMessage
-                {
+                messages.Add(new LocationMessage {
                     type = "location",
                     title = "myLocation",
                     address = "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
                     latitude = 35.65910807942215,
                     longitude = 139.70372892916203
                 });
-                messages.Add(new LocationMessage
-                { 
+                messages.Add(new LocationMessage {
                     type = "location",
                     title = "myLocation",
                     address = "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
@@ -111,7 +113,7 @@ namespace Website.Controllers
                     longitude = 139.70372892916203
                 });
 
-                var postData = new ReplyMessages{
+                var postData = new ReplyMessages {
                     replyToken = replyToken,
                     messages = messages
                 };
@@ -136,23 +138,20 @@ namespace Website.Controllers
                 Console.WriteLine($"TO LINE SERVER: {url}");
                 Console.WriteLine($"requestBody:");
                 Console.WriteLine($"{jsonStr}");
-            }
-            catch(Exception ex)
-            {
+            } catch (Exception ex) {
                 result += "Exception: " + ex.Message;
                 Console.WriteLine($"Exception: {ex.Message}");
             }
             Console.WriteLine($"====================");
             return result;
         }
-        private string ReplyLocationMessages(string replyToken, string address)
-        {
+
+        private string ReplyLocationMessages(string replyToken, string address) {
             string result = "";
-            try
-            {
+            try {
                 // 取得欲傳送的MaskDataList
                 var topMaskDatas = MaskDataHandler.GetTopMaskDatasByComputingDistance(address, 5);
-                
+
                 // create Request
                 string url = "https://api.line.me/v2/bot/message/reply";
                 var request = (HttpWebRequest)WebRequest.Create(url);
@@ -160,54 +159,43 @@ namespace Website.Controllers
                 request.Headers.Add("Content-Type", "application/json");
                 request.Headers.Add("Authorization", "Bearer " + channelAccessToken);
 
-
                 // Set up messages to send
                 var messages = new List<dynamic>();
                 StringBuilder builder = new StringBuilder();
-                
-               
-                
-                if(topMaskDatas.Count == 0)
-                {
+
+                if (topMaskDatas.Count == 0) {
                     string locationSuffix = LocationHandler.GetLocationFirstDivisionSuffix(address);
                     builder.Append($"所在區域({locationSuffix})沒有相關藥局");
-                    messages.Add(new TextMessage
-                    {
+                    messages.Add(new TextMessage {
                         type = "text",
                         text = builder.ToString()
                     });
-                }
-                else
-                {
-                    foreach (var maskData in topMaskDatas)
-                    {
+                } else {
+                    foreach (var maskData in topMaskDatas) {
                         Location location = MapApiHandler.GetGeocoding(maskData.Address).results[0].geometry.location;
 
-                        if (!Double.TryParse(location.lat, out double lat))
-                        {
+                        if (!Double.TryParse(location.lat, out double lat)) {
                             Console.WriteLine($"Ex: Cannot parse {location.lat} to Int.");
                             lat = Double.MinValue;
                         }
 
-                        if (!Double.TryParse(location.lng, out double lng))
-                        {
+                        if (!Double.TryParse(location.lng, out double lng)) {
                             Console.WriteLine($"Ex: Cannot parse {location.lng} to Int.");
                             lng = Double.MinValue;
                         }
 
-                        messages.Add(new LocationMessage
-                        { 
+                        messages.Add(new LocationMessage {
                             type = "location",
                             title = maskData.Name + "\n(成人:" + maskData.AdultMasks + "/兒童: " + maskData.ChildMasks + ")",
                             address = maskData.Address,
                             latitude = lat,
                             longitude = lng
                         });
-                        // builder.Append(maskData.Name + " " + maskData.Address + " " + 
+                        // builder.Append(maskData.Name + " " + maskData.Address + " " +
                         //     maskData.AdultMasks + " " + maskData.ChildMasks + "\n");
                     }
                 }
-               
+
                 // messages.Add(new LocationMessage
                 // {
                 //     type = "location",
@@ -216,9 +204,8 @@ namespace Website.Controllers
                 //     latitude = 35.65910807942215,
                 //     longitude = 139.70372892916203
                 // });
-                
 
-                var postData = new ReplyMessages{
+                var postData = new ReplyMessages {
                     replyToken = replyToken,
                     messages = messages
                 };
@@ -243,9 +230,7 @@ namespace Website.Controllers
                 Stream stream = response.GetResponseStream();
                 StreamReader streamReader = new StreamReader(stream);
                 result += streamReader.ReadToEnd();
-            }
-            catch(Exception ex)
-            {
+            } catch (Exception ex) {
                 result += "Exception: " + ex.Message;
                 Console.WriteLine($"Exception: {ex.Message}");
             }
