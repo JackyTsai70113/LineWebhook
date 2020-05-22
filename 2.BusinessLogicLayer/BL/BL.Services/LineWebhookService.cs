ea77bdd1828252c4ead2003394fc553931cfa6b4
Jacky_Home <j70113jacky@yahoo.com.tw>
2020-05-23 04:06:25 +0800

    no message

--- 2.BusinessLogicLayer/BL/BL.Services/LineWebhookService.cs
+++ 2.BusinessLogicLayer/BL/BL.Services/LineWebhookService.cs
@@ -268,6 +268,14 @@ namespace BL.Services {
                                         new MessageAction() {
                                             label = "Yes",
                                             text = "Yes"
+                                        },
+                                        new MessageAction() {
+                                            label = "Yes2",
+                                            text = "Yes2"
+                                        },
+                                        new MessageAction() {
+                                            label = "Yes3",
+                                            text = "Yes3"
                                         }
                                     }
                                 },
