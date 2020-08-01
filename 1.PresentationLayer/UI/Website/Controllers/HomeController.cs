aea5248d5578a2d3b2f7cba20c32d4d2ea10cceb
JackyTsai <j10926jacky@gmail.com>
2020-08-01 21:38:37 +0800

    修正樣式

--- 1.PresentationLayer/UI/Website/Controllers/HomeController.cs
+++ 1.PresentationLayer/UI/Website/Controllers/HomeController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
-using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Models.MaskDatas;
