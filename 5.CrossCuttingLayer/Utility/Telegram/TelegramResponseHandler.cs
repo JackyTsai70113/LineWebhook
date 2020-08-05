9a0e14ebe4f7a4de7a58b72fd1d7512a7ef70b2e
JackyTsai <j10926jacky@gmail.com>
2020-08-06 02:51:30 +0800

    Telegram bot加上 send message 功能

--- 5.CrossCuttingLayer/Utility/Telegram/TelegramResponseHandler.cs
+++ 5.CrossCuttingLayer/Utility/Telegram/TelegramResponseHandler.cs
@@ -9,6 +9,7 @@ namespace Utility.Telegram {
     /// </summary>
     public class TelegramResponseHandler {
         public static string GetMe() {
+            RequestUtility.GetStringFromGetRequest("https://api.telegram.org/bot1253249749:AAEhPVK8fvahMGCKee_ZtG8fOivf4CjKYsY/sendMessage?chat_id=1017180008&text=HelloText");
             var botClient = new TelegramBotClient("1253249749:AAEhPVK8fvahMGCKee_ZtG8fOivf4CjKYsY");
             var me = botClient.GetMeAsync().Result;
             string result = me.Serialize();
