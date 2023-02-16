b6172236235bb42db90c84a9354b29a31082bfc2
JackyTsaiMac <j10926jacky@gmail.com>
2023-02-17 03:44:52 +0800

    revise reference

--- 2.BusinessLogicLayer/BL.Service.Tests/TWSE_Stock/FakeExchangeRateService.cs
+++ 2.BusinessLogicLayer/BL.Service.Tests/TWSE_Stock/FakeExchangeRateService.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
 using BL.Service.Interface;
-using Core.Domain.DTO.Sinopac;
-using Core.Domain.Utilities;
-using isRock.LineBot;
 
-namespace BL.Service.Sinopac {
-
-    public class FakeExchangeRateService : IExchangeRateService {
-
+namespace BL.Service.Sinopac
+{
+    public class FakeExchangeRateService : IExchangeRateService
+    {
         /// <summary>
         /// 取得換匯資訊
         /// </summary>
@@ -17,9 +11,10 @@ namespace BL.Service.Sinopac {
         /// <param name="bankSellingRate">銀行賣出匯率</param>
         /// <param name="quotedDateTime">報價時間</param>
         public void GetExchangeRate(
-            out double bankBuyingRate, out double bankSellingRate,
-            out DateTime quotedDateTime) {
-
+            out double bankBuyingRate,
+            out double bankSellingRate,
+            out DateTime quotedDateTime)
+        {
             bankBuyingRate = 27.775;
             bankSellingRate = 27.87;
             quotedDateTime = new DateTime(2022, 1, 28, 15, 30, 38);
