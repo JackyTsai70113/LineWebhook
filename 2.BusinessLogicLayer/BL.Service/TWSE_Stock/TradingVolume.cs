1af055cf9db99d8caff16d6bcfcaee2e5e81d3c3
jacky_tsai <j10926jacky@gmail.com>
2025-01-28 13:53:31 +0800

    feat: revise TradingVolume

--- 2.BusinessLogicLayer/BL.Service/TWSE_Stock/TradingVolume.cs
+++ 2.BusinessLogicLayer/BL.Service/TWSE_Stock/TradingVolume.cs
@@ -1,14 +1,23 @@
-﻿namespace BL.Service.TWSE_Stock
+﻿using System.Text.Json.Serialization;
+
+namespace BL.Service.TWSE_Stock
 {
 
     public class TradingVolume
     {
+        [JsonPropertyName("stat")]
         public string Stat { get; set; }
+        [JsonPropertyName("date")]
         public string Date { get; set; }
+        [JsonPropertyName("title")]
         public string Title { get; set; }
+        [JsonPropertyName("fields")]
         public string[] Fields { get; set; }
+        [JsonPropertyName("data")]
         public string[][] Data { get; set; }
+        [JsonPropertyName("notes")]
         public string[] Notes { get; set; }
+        [JsonPropertyName("groups")]
         public Group[] Groups { get; set; }
     }
 
